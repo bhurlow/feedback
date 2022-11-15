fa3c3ccecf5feba4ada106e1f84664ceb6c6566b
Brian Hurlow <brian@brianhurlow.com>
2022-11-14 21:03:21 -0500

    deno pg app setup

---
+++ main.ts
@@ -0,0 +1,27 @@
+import { app, get } from "https://denopkg.com/syumai/dinatra/mod.ts";
+
+import { Client } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
+
+const client = new Client(Deno.env.get("DATABASE_URL"));
+
+const greeting = "<h1>Hello From Deno on Fly!</h1>";
+
+app(
+  get("/", () => greeting),
+  get("/:id", async ({ params }) => {
+    console.log(params);
+
+    await client.connect();
+
+    const array_result = await client.queryArray(
+      "SELECT * FROM pg_stat_activity",
+    );
+
+    console.log(array_result);
+
+    return 'ret';
+
+    // console.log(array_result.rows);
+    // return '<h1> sup </h1>'
+  }),
+);
